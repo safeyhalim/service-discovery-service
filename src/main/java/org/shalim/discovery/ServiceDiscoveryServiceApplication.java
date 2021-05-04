29f59f3be6dab37a5aafad44466d1e4b3b539fa4
Halim <safey.halim@1886.ventures>
2021-05-04 13:13:25 +0200

    Project basic structure

---
+++ src/main/java/org/shalim/discovery/ServiceDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package org.shalim.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryServiceApplication.class, args);
+	}
+
+}
